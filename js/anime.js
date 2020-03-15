72d0bbe3462dabf1959736459783e32c1880e581
Gustavo Santos <gusta99sant@gmail.com>
2020-03-15 03:03:21 -0300

    Alterações na página photography, aplicando responsabilidade.

---
+++ js/anime.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+  // 4 FOTOS EM COLUNA
+  $( ".anime-right" ).addClass( "anime-start" );
+  $( ".anime-top" ).addClass( "anime-start" );
+  $( ".anime-bottom" ).addClass( "anime-start" );
+  $( ".anime-left" ).addClass( "anime-start" );
+});
